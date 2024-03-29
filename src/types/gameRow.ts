3b3e547a3bed2fdfe4c7ee7629d5e92f738d6e29
Magulloff <przemczar2001@gmail.com>
2024-03-29 22:13:57 +0100

    add game types

---
+++ src/types/gameRow.ts
@@ -0,0 +1,10 @@
+import { Position } from "./position";
+import { ResponseResult } from "./responseResult";
+
+export type GameRow = {
+  cuePosition: Position;
+  targetPosition: Position;
+  isCueValid: boolean;
+  responsTimeMs: number;
+  result: ResponseResult;
+};
---
+++ src/types/responseResult.ts
@@ -0,0 +1,1 @@
+export type ResponseResult = "correct" | "wrong" | "overtime";
